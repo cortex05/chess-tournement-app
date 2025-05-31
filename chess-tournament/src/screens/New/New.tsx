488171c7273a2c3dfcd0d4bdcc5eb30141d39728
cortex05 <havinginitiative@gmail.com>
2025-05-31 13:44:20 -0500

    feat: modal opening

--- chess-tournament/src/screens/New/New.tsx
+++ chess-tournament/src/screens/New/New.tsx
@@ -1,15 +1,60 @@
-import { useState } from "react";
-import { Stack, Button } from "@mui/material";
-import { Header } from "../../components/Header/Header";
+import { useActionState, useState } from "react";
+import { Stack, Button, Box } from "@mui/material";
 import styles from "./New.module.css";
 import { PersonAddAltSharp } from "@mui/icons-material";
+import Modal from '@mui/material/Modal';
+
+import { Header } from "../../components/Header/Header";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%',
+//   width: 600,
+//   bgcolor: 'black',
+//   boxShadow: 24,
+//   border: '2px solid #000'
+// }
 
 const New = () => {
   const [playerCount, setPlayerCount] = useState<number>(0);
   const [roster, setRoster] = useState<string[]>([]);
+  const [isFinished, setIsFinished] = useState<boolean>(false)
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+
+  const handleAdd = () => {
+    setIsModalOpen(false)
+  }
+
   return (
     <>
       <Header lossCheck={true} />
+      <Modal 
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className={styles.modal}>
+          <div className={styles.modal}>
+          {/* <Box sx={style}> */}
+            <h3>Add player</h3>
+            <Button onClick={() => handleAdd()}>Add Player</Button>
+          {/* </Box> */}
+          </div>
+      </Modal>
       <section className={styles.main}>
         <h1>New Tournament</h1>
         <h4>
@@ -22,6 +67,7 @@ const New = () => {
               variant="outlined"
               startIcon={<PersonAddAltSharp />}
               size="large"
+              onClick={() => setIsModalOpen(true)}
             >
               Add Player
             </Button>
